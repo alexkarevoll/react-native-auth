3acf6463d1e76b09d4822f983a78faaef57faa14
alexkarevoll <alex.karevoll@gmail.com>
2017-04-03 14:48:59 -0700

    user logged in state

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
 
   componentWillMount() {
     firebase.initializeApp({
@@ -15,6 +18,14 @@ class App extends Component {
         projectId: 'auth-84d00',
         storageBucket: 'auth-84d00.appspot.com',
         messagingSenderId: '280769826987'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
